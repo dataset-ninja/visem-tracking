473eef50b9f8da93394caa6da37d9da58a05fc21
peixed351 <peixed351@gmail.com>
2024-02-07 12:29:18 +0300

    release

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "VISEM-Tracking"
 PROJECT_NAME_FULL: str = "VISEM-Tracking: Human Spermatozoa Tracking Dataset"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
